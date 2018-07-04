137c876c217181700f75cc057d294b3bc8ef4997
Duah Kwaku <ibrahimkwakuduah@hotmail.com>
2018-07-04 00:25:09 +0000

    Classes Controller done

--- TeachingAssistantUltimate/Context/ApplicationDbContext.cs
+++ TeachingAssistantUltimate/Context/ApplicationDbContext.cs
@@ -27,6 +27,12 @@ namespace TeachingAssistantUltimate.Context
                 new Subjects { Subject = "Public Health Nursing", SubjectsID = 1, SubjectCode = "001" },
                 new Subjects { Subject = "Basic Infection Prevention and Control", SubjectsID = 2, SubjectCode = "002" }
                 ));
+
+            builder.Entity<AssessmentTypes>(x => x.HasData(
+                new AssessmentTypes { AssessmentTypesID = 1, AssessmentType = "Quiz", Total = 10 },
+                new AssessmentTypes { AssessmentTypesID = 2, AssessmentType = "Assignment", Total = 10 },
+                new AssessmentTypes { AssessmentTypesID = 3, AssessmentType = "Mid-Sem", Total = 20 }
+                ));
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.// For example, you can rename the ASP.NET Identity table names and more.// Add your customizations after calling base.OnModelCreating(builder);
         }
@@ -36,5 +42,13 @@ namespace TeachingAssistantUltimate.Context
         public virtual DbSet<Subjects> Subjects { get; set; }
 
         public virtual DbSet<Options> Options { get; set; }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        public virtual DbSet<Classes> Classes { get; set; }
+
+        public virtual DbSet<AssessmentTypes> AssessmentTypes { get; set; }
+
+        public virtual DbSet<Results> Results { get; set; }
     }
 }
--- TeachingAssistantUltimate/Migrations/20180704001549_ForgottenTables.Designer.cs
+++ TeachingAssistantUltimate/Migrations/20180704001549_ForgottenTables.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeachingAssistantUltimate.Context;
 
 namespace TeachingAssistantUltimate.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180704001549_ForgottenTables")]
+    partial class ForgottenTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
